43b5fe1d44a9d6d950d5a0d5940dcc8bd1814c2a
Jamis <ngjames010@gmail.com>
2024-11-08 15:31:11 +0800

    chore: init

---
+++ frontend/src/pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the main landing page of the application.</p>
+
+      <button>Second Page</button>
+    </div>
+  );
+}
+
+export default Home;
